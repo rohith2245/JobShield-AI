78efbf46f7898fc75d66b00b722211aaa2e1c3d6
Rohith Veeramalla <rohithveeramalla@gmail.com>
2026-05-28 12:27:48 +0530

    Production PostgreSQL deployment setup

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
 import os
 
 class Config:
-    SECRET_KEY = "jobshield_secret_key"
 
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    SECRET_KEY = os.getenv(
+        "SECRET_KEY",
+        "jobshield_secret_key"
+    )
 
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database', 'jobshield.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///jobshield.db"
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
